0244d1c115c9156265630995dc2bd710e170266c
viktor-dimalanta <dimalantaviktorangelo@gmail.com>
2021-06-04 07:33:08 +0800

    template change

--- view/team/list.php
+++ view/team/list.php
@@ -16,15 +16,15 @@
         <tr>
             <td><?= $row['code'] ?></td>
             <td><?= $row['name'] ?></td>
-            <td><a href="http://localhost/pdomvc/index.php/team/detail?id=<?= $row['id'] ?>" class="btn btn-success btn-xs"> Detail</a></td>
-            <td><a href="http://localhost/pdomvc/index.php/team/edit?id=<?= $row['id'] ?>" class="btn btn-warning btn-xs"><span class="glyphicon glyphicon-edit"></span> Edit</a></td>
-            <td><a href="http://localhost/pdomvc/index.php/team/delete?id=<?= $row['id']?>" onclick="return confirm('Anda yakin akan menghapus data ini?')" class="btn btn-danger btn-xs"> <span class="glyphicon glyphicon-trash"></span> Delete</a></td>
+            <td><a href="http://localhost/advanced-php-test/index.php/team/detail?id=<?= $row['id'] ?>" class="btn btn-success btn-xs"> Detail</a></td>
+            <td><a href="http://localhost/advanced-php-test/index.php/team/edit?id=<?= $row['id'] ?>" class="btn btn-warning btn-xs"><span class="glyphicon glyphicon-edit"></span> Edit</a></td>
+            <td><a href="http://localhost/advanced-php-test/index.php/team/delete?id=<?= $row['id']?>" onclick="return confirm('Anda yakin akan menghapus data ini?')" class="btn btn-danger btn-xs"> <span class="glyphicon glyphicon-trash"></span> Delete</a></td>
         </tr>
         <?php endforeach ?>
     </table>
 	</div>
     <br>
-    <a href="http://localhost/pdomvc/index.php/team/create" class="btn btn-primary btn-sm"><span class="glyphicon glyphicon-plus"></span> Create</a>
+    <a href="http://localhost/advanced-php-test/index.php/team/create" class="btn btn-primary btn-sm"><span class="glyphicon glyphicon-plus"></span> Create</a>
 <?php $isi = ob_get_clean() ?>
 
 <?php include 'view/template.php' ?>
